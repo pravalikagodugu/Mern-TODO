f2a366a22cc0da9f21d5e518c0c75495171fde29
Pravalika Godugu <128896984+pravalikagodugu@users.noreply.github.com>
2024-12-11 14:30:04 +0530

    Initial commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -51,7 +51,7 @@ export default function App() {
 
   return (
     <main className="container">
-      <h1 className="title">Awesome Todos</h1>
+      <h1 className="title">Todo List</h1>
       <form className="form" onSubmit={createNewTodo}>
         <input
           type="text"
